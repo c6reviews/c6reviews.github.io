8b0e93f95a0e20105f3be4047a8cf047aefb43c8
delvareus <c6reviews@gmail.com>
2025-09-16 14:30:25 -0400

    tts-troubleshoot-2

--- tts.js
+++ tts.js
@@ -54,7 +54,8 @@ function textToSpeech(id,text) {
 		setTimeout(() => {
 			var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 			if (isSafari){
-				utterance.voice = speechSynthesis.getVoices()[0];
+				let voices = speechSynthesis.getVoices();
+				document.getElementById('svoices').innerHTML = voices;
 			} else {
 				utterance.voice = speechSynthesis.getVoices()[1];
 			}
